d0b32dcd1dac288bb10a35618edb616feb4da3ad
Steven Maude <steve@on.ubuntu>
2013-10-07 16:05:08 +0100

    Initial commit; basic functionality.

---
+++ unshort.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+#TODO: Autogenerate API key if one does not exist
+
+def unshort(url):
+    """
+    Take a url as string and returned unshorted form using unshort.me API.
+    """
+    # TODO: get API key
+    query_url = 'http://api.unshort.me/unshorten/v2/?r={0}\
+                 &format={1}'.format(url, 'json')
+    r = requests.get(query_url)
+    output_json = json.loads(r.text)
+    return output_json['resolvedURL']
+
