18b4eeca7b6f9fda34fe634b9dd794c2b7b244bd
Steven Maude <steve@github.ubuntu>
2013-11-13 15:32:23 +0000

    Add ability to load user provided API key; eventually should autogenerate this if no key exists.

--- unshort.py
+++ unshort.py
@@ -12,21 +12,55 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
+import json
+import sys
+import logging
 
 import requests
-import json
 
-#TODO:
-#Autogenerate API key if one does not exist
-#Exception handling
+
+#TODO: Exception handling
 def unshort(url):
     """
     Take a url as string and returned unshorted form using unshort.me API.
     """
-    # TODO: get API key
+    try:
+        api_key = load_api_key()
+        logging.info("Loaded API key: {}".format(api_key))
+    except IOError:
+    # TODO: create API key
+        create_api_key()
+        load_api_key()
+
     query_url = 'http://api.unshort.me/unshorten/v2/?r={0}\
-                 &format={1}'.format(url, 'json')
+                 &format={1}&api_key={2}'.format(url, 'json', api_key)
     r = requests.get(query_url)
     output_json = json.loads(r.text)
-    return output_json['resolvedURL']
+    try:
+        return output_json['resolvedURL']
+    except KeyError:
+        logging.warning("Error resolving URL: {}".format(url))
+        return output_json['error']
+
+
+def load_api_key(api_key_filename='api_key'):
+    """
+    Load unshort.me API key
+    """
+    with open(api_key_filename) as api_key_file:
+        return api_key_file.read().strip()
+
+
+def create_api_key():
+    """
+    Create unshort.me API key
+    """
+    pass
+
+
+def store_api_key():
+    """
+    Store API key to api_key file.
+    """
+    pass
 
